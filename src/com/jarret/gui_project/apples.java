9888a5e3709aad502313a54b8a5ca06a63001b28
finit3Loop <37601914+finit3Loop@users.noreply.github.com>
2018-12-09 22:57:31 -0600

    Continued through 55

--- src/com/jarret/gui_project/apples.java
+++ src/com/jarret/gui_project/apples.java
@@ -6,23 +6,34 @@ public class apples {
 
 	public static void main(String[] args) {
 
-		if (1 == 1) {
+		if (true) {
 			System.out.println("hello");
 		}
 
+		//java GUI 1.0
 		/*
 		 * for (int i = 1; i<9999999;i++) { System.out.println(i); }
 		 */
-
-		String fn = JOptionPane.showInputDialog("Enter first number");
-		String sn = JOptionPane.showInputDialog("Enter second number");
-
-		int num1 = Integer.parseInt(fn);
-		int num2 = Integer.parseInt(sn);
-
-		int sum = num1 + num2;
-
-		JOptionPane.showMessageDialog(null, "The answer is " + sum, "the title", JOptionPane.PLAIN_MESSAGE);
+		/*
+		 * String fn = JOptionPane.showInputDialog("Enter first number"); String sn =
+		 * JOptionPane.showInputDialog("Enter second number");
+		 * 
+		 * int num1 = Integer.parseInt(fn); int num2 = Integer.parseInt(sn);
+		 * 
+		 * int sum = num1 + num2;
+		 * 
+		 * while (1 == 1) { JOptionPane.showMessageDialog(null, "The answer is " + sum,
+		 * "the title", JOptionPane.PLAIN_MESSAGE); }
+		 */
+		
+		
+		//Continuing GUI 1.1
+		tuna bucky = new tuna();
+		
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bucky.setSize(350, 100);
+		bucky.setVisible(true);
+		
 	}
 
 }
